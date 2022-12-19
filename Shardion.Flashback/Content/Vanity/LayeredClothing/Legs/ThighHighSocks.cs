40254b832ccad18f2e7b3ce345648f9174d393d7
Shardion <22127191+Shardion@users.noreply.github.com>
2022-12-18 23:04:47 -0800

    yes this is the test case

---
+++ Shardion.Flashback/Content/Vanity/LayeredClothing/Legs/ThighHighSocks.cs
@@ -0,0 +1,13 @@
+using Terraria.ModLoader;
+using Shardion.Flashback.Internal;
+
+namespace Shardion.Flashback.Content.Vanity.LayeredClothing.Legs
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class ThighHighSocks : VanityItem
+    {
+        // flashback is so good I don't even need to put anything in this class
+        // TODO: consider a hacky reflection way to automatically generate these classes from asset locations
+        //       even more flexing and even less code
+    }
+}
