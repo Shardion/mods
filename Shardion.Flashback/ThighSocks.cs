f156baf76e2b25b74bc97f6cf9123a9c87afc241
Shardion <22127191+Shardion@users.noreply.github.com>
2022-12-18 23:14:25 -0800

    empty files for planned items

---
+++ Shardion.Flashback/ThighSocks.cs
@@ -0,0 +1,10 @@
+using Terraria.ModLoader;
+using Shardion.Flashback.Internal;
+
+namespace Shardion.Flashback.Content.Vanity.LayeredClothing.Legs
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class ThighSocks : VanityItem
+    {
+    }
+}
