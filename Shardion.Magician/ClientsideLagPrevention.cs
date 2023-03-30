e4c08e8ea8754df3b52204bddf3806529193e0d3
Shardion <22127191+Shardion@users.noreply.github.com>
2023-03-30 14:23:38 -0700

    broken magician changes, gitignore update

--- Shardion.Magician/ClientsideLagPrevention.cs
+++ Shardion.Magician/ClientsideLagPrevention.cs
@@ -16,10 +16,12 @@ namespace Shardion.Magician
     {
         public static bool BossAlive { get; set; }
         public static bool DoFullbright { get; set; }
+        public static bool DoItemCull { get; set; }
         public static BossConfigurable DoDustDeletion { get; set; }
         public static BossConfigurable DoCombatTextDeletion { get; set; }
         public static BossConfigurable DoGoreDeletion { get; set; }
         public static BossConfigurable DoRainDeletion { get; set; }
+        public static BossConfigurable DoItemHide { get; set; }
     }
 
     public class ClientsideLagPreventionConfig : ModConfig
@@ -27,24 +29,50 @@ namespace Shardion.Magician
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
         [DefaultValue(true)]
+        [DisplayName("Fullbright if a boss is alive")]
+        [Tooltip("Disable the lighting engine when a boss is alive.")]
         public bool FullbrightWhenBossAlive { get; set; }
+
+        [DefaultValue(true)]
+        [DisplayName("Cull dropped items")]
+        [Tooltip("Only draw dropped items if they are on-screen.")]
+        public bool ItemCulling { get; set; }
+
         [DefaultValue(BossConfigurable.IfBossAlive)]
+        [DisplayName("Delete dust")]
+        [Tooltip("Delete dust as soon as it spawns.")]
         public BossConfigurable DeleteDust { get; set; }
+
         [DefaultValue(BossConfigurable.IfBossAlive)]
+        [DisplayName("Delete combat text")]
+        [Tooltip("Delete combat text as soon as it spawns.")]
         public BossConfigurable DeleteCombatText { get; set; }
+
         [DefaultValue(BossConfigurable.IfBossAlive)]
+        [DisplayName("Delete gore")]
+        [Tooltip("Delete gore as soon as it spawns.")]
         public BossConfigurable DeleteGore { get; set; }
+
         [DefaultValue(BossConfigurable.IfBossAlive)]
+        [DisplayName("Delete rain")]
+        [Tooltip("Delete rain as soon as it spawns.")]
         public BossConfigurable DeleteRain { get; set; }
+
+        [DefaultValue(BossConfigurable.IfBossAlive)]
+        [DisplayName("Hide dropped items")]
+        [Tooltip("Disable drawing of dropped items.")]
+        public BossConfigurable HideItems { get; set; }
 
         public override void OnChanged()
         {
             base.OnChanged();
             ClientsideLagPrevention.DoFullbright = FullbrightWhenBossAlive;
+            ClientsideLagPrevention.DoItemCull = ItemCulling;
             ClientsideLagPrevention.DoDustDeletion = DeleteDust;
             ClientsideLagPrevention.DoCombatTextDeletion = DeleteCombatText;
             ClientsideLagPrevention.DoGoreDeletion = DeleteGore;
             ClientsideLagPrevention.DoRainDeletion = DeleteRain;
+            ClientsideLagPrevention.DoItemHide = HideItems;
         }
     }
 
@@ -69,5 +97,13 @@ namespace Shardion.Magician
                 _npcTick = 0;
             }
         }
+    }
+}
+
+// dummy to make tML happy
+namespace ClientsideLagPrevention
+{
+    public class ClientsideLagPrevention
+    {
     }
 }
